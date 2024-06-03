4d29bc0b4b3cd7923f038b356bbaca3f79082b67
lamnthe172601 <lamnthe172601@fpt.edu.vn>
2024-06-04 03:00:50 +0700

    up

--- src/java/AccountControll/LoginController.java
+++ src/java/AccountControll/LoginController.java
@@ -4,7 +4,9 @@
  */
 package AccountControll;
 
-import dal.DAO;
+import Models.Company;
+import Models.Recruiter;
+import dal.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +15,8 @@ import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import Models.User;
+import dal.CompanyDAO;
+import dal.RecruiterDAO;
 
 /**
  *
