6a55e2b42d43eee58c3aa401d291048d200d8307
lamnthe172601 <lamnthe172601@fpt.edu.vn>
2024-06-12 13:46:50 +0700

    update

--- src/java/MutiDAO/FreelancerInformationDAO.java
+++ src/java/MutiDAO/FreelancerInformationDAO.java
@@ -29,7 +29,27 @@ public class FreelancerInformationDAO extends DBContext{
             while (rs.next()) {
                User u = new User(rs.getInt("userID"),rs.getString("username"),rs.getString("password"),rs.getString("email"), rs.getString("status"),new Role(rs.getInt("roleID"), rs.getString("role_name")),rs.getBoolean("LevelPass"));
                Freelancer f = new Freelancer(rs.getInt("freelanceID"), rs.getString("first_name"),rs.getString("last_name"),rs.getString("image"),rs.getBoolean("gender"),rs.getDate("dob"),rs.getString("describe"),rs.getString("email__contact"),rs.getString("phone_contact"));
-               list.add(new FreelancerInformation(u, f) );
+               list.add(new FreelancerInformation(u, f,get_ListSkillsByFreelancerId(rs.getInt("freelanceID"))) );
+            }
+            return list;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;  
+    }
+    
+     public List<SkillSet> get_ListSkillsByFreelancerId(int freelancerid){
+       List<SkillSet> list = new ArrayList<>();
+        String query = """
+                          select * from Skill_Set ss join Skills s on ss.skill_set_ID=s.skill_set_ID
+                          where s.freelancerID = ? """;
+        try {
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setInt(1,freelancerid);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+              SkillSet s = new SkillSet(rs.getInt("skill_set_ID"),rs.getString("skill_set_name"));
+              list.add(s);
             }
             return list;
         } catch (SQLException e) {
@@ -38,6 +58,6 @@ public class FreelancerInformationDAO extends DBContext{
         return list;  
     }
     public static void main(String[] args) {
-        System.out.println(new FreelancerInformationDAO().get_ListFreelancer());
+        System.out.println(new FreelancerInformationDAO().get_ListSkillsByFreelancerId(1).get(0).getSkill_set_name());
     }
 }
