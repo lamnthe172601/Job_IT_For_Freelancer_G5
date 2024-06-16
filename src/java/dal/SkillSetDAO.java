7c48df18989cc45b441c8cac84a6699c49f89257
LinhND-he172819 <linhndhe172819@fpt.edu.vn>
2024-06-17 02:33:33 +0700

    u

---
+++ src/java/dal/SkillSetDAO.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author DUC MINH
+ */
+public class SkillSetDAO extends DBContext{
+    public List<Map<String, String>> getAllSkillSets() {
+        List<Map<String, String>> skillSets = new ArrayList<>();
+        String sql = "SELECT skill_set_ID, skill_set_name, description FROM Skill_Set";
+        
+        try (Connection conn = connection; 
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+             
+            while (rs.next()) {
+                Map<String, String> skillSet = new HashMap<>();
+                skillSet.put("skill_set_ID", String.valueOf(rs.getInt("skill_set_ID")));
+                skillSet.put("skill_set_name", rs.getString("skill_set_name"));
+                skillSet.put("description", rs.getString("description"));
+                skillSets.add(skillSet);
+            }
+        } catch (Exception e) {
+        }
+        return skillSets;
+    }
+}
